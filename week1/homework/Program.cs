1cee4c16782540934633ddeec2fb10de854eb51a
sumanghimire79 <sumanghimire79@yahoo.com>
2023-02-24 22:59:39 +0100

    homework week1_exercise2

--- week1/homework/Program.cs
+++ week1/homework/Program.cs
@@ -24,4 +24,28 @@ app.MapGet("/1", () =>
   return ($"Reversed input value: {reversed}");
 });
 
+//2. String/Math
+// Return the number (count) of vowels in the given string. Consider a, e, i, o, u as vowels.
+app.MapGet("/2", () =>
+{
+  string input = "IntellectualizationOOYa";
+  int vowelCount = GetVowelCount(input.ToLower()); //TODO: Implement GetVowelCount
+
+  int GetVowelCount(string word)
+  {
+    int count = 0;
+    foreach (char c in word)
+    {
+      if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+      {
+        count += 1;
+      }
+    }
+    return count;
+  }
+  Console.WriteLine($"Number of vowels: {vowelCount}");
+  return ($"Number of vowels: {vowelCount}");
+});
+
+
 app.Run();
