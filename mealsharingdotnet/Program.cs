0d98c46f946c1a392d57f38f19b616c8d18c963f
sumanghimire79 <sumanghimire79@yahoo.com>
2023-03-22 23:50:03 +0100

    Added mealsharing post and get method

---
+++ mealsharingdotnet/Program.cs
@@ -0,0 +1,89 @@
+
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IMealService, FileMealService>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "week5-mealsharing dotnet");
+
+app.MapPost("/meals", ([FromServices] IMealService mealSharingService, Meal meal) =>
+{/*eg: postman input
+  {
+    "headline": "Nepal Hot Pot",
+        "imageURL": "https://askme.com",
+        "bodyText": "popular dumpling",
+        "location": "ishoj,cph",
+        "price": 500
+}*/
+  mealSharingService.AddMeal(meal);
+});
+
+app.MapGet("/meals", ([FromServices] IMealService mealSharingService) =>
+{
+  return mealSharingService.ListMeals();
+});
+
+app.Run();
+
+public interface IMealService
+{
+  List<Meal> ListMeals();
+  void AddMeal(Meal meal);
+}
+public class Meal
+{
+  public int ID { get; set; }
+  public string? Headline { get; set; }
+  public string? ImageURL { get; set; }
+  public string? BodyText { get; set; }
+  public string? Location { get; set; }
+  public int Price { get; set; }
+}
+public class FileMealService : IMealService
+{
+  // public List<Meal> listOfMeals = new List<Meal>();
+  public List<Meal> ListMeals()
+  {
+    var readJsonFile = File.ReadAllText(@"meals.json");
+    var mealsFromJsonFile = System.Text.Json.JsonSerializer.Deserialize<List<Meal>>(readJsonFile);
+    // return listOfMeals;
+    return mealsFromJsonFile;
+  }
+  public void AddMeal(Meal meal)
+  {
+    if (!File.Exists("meals.json"))
+    {
+      File.WriteAllText("meals.json", "[]");
+    }
+    var readJsonFile = File.ReadAllText(@"meals.json");
+    var meals = System.Text.Json.JsonSerializer.Deserialize<List<Meal>>(readJsonFile);
+    // listOfMeals.Add(meal);
+    if (meals.Count == 0)
+    {
+      meal.ID = 1;
+    }
+    if (meal.ID == 0)
+    {
+      meal.ID = meals[meals.Count - 1].ID + 1;
+    }
+    foreach (var m in meals)
+    {
+      if (m.ID == meal.ID)
+      {
+        throw new Exception($"{meal.ID} already exists");
+      }
+      else if (m.ID != m.ID)
+      {
+        meal.ID = meals[meals.Count - 1].ID + 1;
+      }
+    }
+    meals.Add(meal);
+    var mealsJson = System.Text.Json.JsonSerializer.Serialize(meals);
+    File.WriteAllText("meals.json", mealsJson);
+    Console.WriteLine(mealsJson);
+  }
+
+}
