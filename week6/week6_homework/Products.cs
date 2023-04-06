75ad956541e04624eda144362ef1b720e546d9e3
sumanghimire79 <sumanghimire79@yahoo.com>
2023-04-06 23:03:18 +0200

    Added dapper CRUD operation for both products and users mysql table

--- week6/week6_homework/Products.cs
+++ week6/week6_homework/Products.cs
@@ -7,26 +7,27 @@ namespace HackYourFuture.Week6;
 
 public interface IProductRepository
 {
+  Task<Object> AddProducts(Product product);
   Task<IEnumerable<Product>> GetProducts();
+  Task<Object> GetProductById(int id);
+  Task<Object> DeleteProductById(int id);
+  Task<Object> UpdatedProductById(int id, Product product);
 }
 public class Product
 {
-  // public string _ID;
-  // public string ID { get => _ID; set => new Guid(); }
-  public string ID { get; set; }
-  public string? ProductName { get; set; }
+  public int ID { get; set; }
+  public string? Name { get; set; }
   public decimal Price { get; set; }
   public string? Description { get; set; }
   public Product() { }
-  public Product(string id, string name, decimal price, string description)
+  public Product(int id, string name, decimal price, string description)
   {
     ID = id;
-    ProductName = name;
+    Name = name;
     Price = price;
     Description = description;
   }
 }
-
 
 public class ProductRepository : IProductRepository
 {
@@ -37,12 +38,69 @@ public class ProductRepository : IProductRepository
     this.connectionString = configuration.GetConnectionString("Default");
   }
 
+
+
+  public async Task<Object> AddProducts(Product product)
+  {
+    using var connection = new MySqlConnection(connectionString);
+    var productId = await connection.QuerySingleAsync<int>(@"INSERT INTO dapper.products (name, price,description) VALUES (@name, @price, @description);
+    SELECT LAST_INSERT_ID();
+", product);
+    var products = await connection.QueryAsync<Product>($"SELECT id, name, price,description FROM dapper.products where id={productId}");
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $" product is posted",
+      AddedProduct = products,
+    });
+  }
+
+
   public async Task<IEnumerable<Product>> GetProducts()
   {
     using var connection = new MySqlConnection(connectionString);
-
-    var products = await connection.QueryAsync<Product>("SELECT id as ID,  name as  ProductName, price as Price , description as Description FROM week6homework.product");
+    var products = await connection.QueryAsync<Product>("SELECT id,  name, price , description FROM dapper.products");
     return products;
   }
 
+  public async Task<Object> GetProductById(int id)
+  {
+    using var connection = new MySqlConnection(connectionString);
+    var productById = await connection.QueryAsync<Product>($"SELECT id, name, price , description FROM dapper.products WHERE id = {id}");
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $"The product that has id {id} is: ",
+      ProductById = productById,
+    });
+  }
+
+  public async Task<Object> UpdatedProductById(int id, Product product)
+  {
+    await using var connection = new MySqlConnection(connectionString);
+    var productUpdated = await connection.ExecuteAsync($"UPDATE dapper.products SET name=@name, price=@price,description=@description WHERE id={id}", product);
+    var UpdatedProduct = await connection.QueryAsync<Product>($"SELECT id, name, price,description FROM dapper.products where id={id}");
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $"this product id:{id} is updated",
+      UpdatedProduct = UpdatedProduct,
+    });
+  }
+
+  public async Task<Object> DeleteProductById(int id)
+  {
+    await using var connection = new MySqlConnection(connectionString);
+    var deletedProduct = await connection.ExecuteAsync("DELETE FROM dapper.products WHERE id=@id", new { ID = id });
+
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $"this product id:{id} is deleted",
+      DeletedProduct = deletedProduct,
+    });
+  }
+
 }
+
+
--- week6/week6_homework/Users.cs
+++ week6/week6_homework/Users.cs
@@ -6,20 +6,23 @@ namespace HackYourFuture.Week6;
 public interface IUserRepository
 {
   Task<IEnumerable<User>> GetUsers();
-  // Task<IEnumerable<User>> AddUsers();
+  Task<Object> GetUserByID(int id);
+  Task<Object> AddUser(User user);
+  Task<Object> DeleteUserByID(int id);
+  Task<Object> UpdateUserByID(int id, User user);
 
 }
 
 public class User
 {
 
-  public string ID { get; set; } = "default id";
-  public string Name { get; set; } = "Default name";
-  public string Email { get; set; } = "default@gmial.com";
-  public string Address { get; set; } = "biratnagar";
+  public int ID { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Address { get; set; }
 
   public User() { }
-  public User(string id, string name, string email, string address)
+  public User(int id, string name, string email, string address)
   {
     ID = id;
     Name = name;
@@ -46,12 +49,55 @@ public class UserRepository : IUserRepository
   }
 
 
-  public async Task<IEnumerable<User>> AddUsers()
+  public async Task<Object> AddUser(User user)
   {
     using var connection = new MySqlConnection(connectionString);
+    var userId = await connection.QuerySingleAsync<int>(@"INSERT INTO week6homework.user (name, email,address) VALUES ( @name, @email, @address);
+        SELECT LAST_INSERT_ID();
+    ", user);
 
-    var users = await connection.QueryAsync<User>("SELECT id as _id, Name as name , Email as email , Address as address FROM week6homework.user");
-    return users;
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $" product is posted",
+      AddedProduct = user,
+    });
+  }
+  public async Task<Object> GetUserByID(int id)
+  {
+    await using var connection = new MySqlConnection(connectionString);
+    var userById = await connection.QueryAsync<User>($"SELECT id as ID, name as Name , email as Email , address as Address FROM week6homework.user WHERE id={id}");
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $" The user that has id: {id} is:",
+      UserByID = userById,
+    });
+  }
+
+  public async Task<Object> UpdateUserByID(int id, User user)
+  {
+    await using var connection = new MySqlConnection(connectionString);
+    var userToUpdated = await connection.ExecuteAsync($"UPDATE week6homework.user SET name=@name, email=@email,address=@address WHERE id={id}", user);
+    var UpdatedUser = await connection.QueryAsync<User>($"SELECT id, name, email,address FROM week6homework.user where id={id}");
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $"this user id:{id} is updated",
+      UpdatedUser = UpdatedUser,
+    });
+  }
+  public async Task<Object> DeleteUserByID(int id)
+  {
+    await using var connection = new MySqlConnection(connectionString);
+    var deletedUser = await connection.ExecuteAsync("DELETE FROM week6homework.user WHERE id=@id", new { ID = id });
+
+    return Results.Ok(new
+    {
+      Status = "200 ok",
+      Message = $"this user id:{id} is deleted",
+      DeletedUser = deletedUser,
+    });
   }
 
 }
