43575c5d969baf3648216130c5cd87abbc25b6ff
sumanghimire79 <sumanghimire79@yahoo.com>
2023-04-10 22:18:56 +0200

    Add review CRUD

--- week7/meal-sharing-final/backend/Program.cs
+++ week7/meal-sharing-final/backend/Program.cs
@@ -2,14 +2,19 @@
 using mealSharingFfinal.meal.meal;
 using mealSharingFfinal.meal.MealRepository;
 using mealSharingFfinal.meal.IMealRepository;
+using mealSharingFfinal.review.review;
+using mealSharingFfinal.review.ReviewRepository;
+using mealSharingFfinal.review.IReviewRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IMealRepository, MealRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "week 7 meal sharing final!");
 
+//meal routes
 app.MapGet("/api/meals", async (IMealRepository mealRepository) =>
 {
   return await mealRepository.GetAllMeal();
@@ -38,6 +43,38 @@ app.MapPut("/api/meals/{id}", async (IMealRepository mealRepository, Meal meal, 
 app.MapDelete("/api/meals/{id}", async (IMealRepository mealRepository, int id) =>
 {
   return await mealRepository.DeleteMealByID(id);
+});
+
+
+//review routes
+app.MapGet("/api/reviews", async (IReviewRepository reviewRepository) =>
+{
+  return await reviewRepository.GetAllReview();
+});
+
+app.MapGet("/api/reviews/search/", async (IReviewRepository reviewRepository, string title) =>
+{
+  return await reviewRepository.SearchReview(title);
+});
+
+app.MapGet("/api/reviews/{id}", async (IReviewRepository reviewRepository, int id) =>
+{
+  return await reviewRepository.GetReviewByID(id);
+});
+
+app.MapPost("/api/reviews", async (IReviewRepository reviewRepository, Review review) =>
+{
+  return await reviewRepository.AddReview(review);
+});
+
+app.MapPut("/api/reviews/{id}", async (IReviewRepository reviewRepository, Review review, int id) =>
+{
+  return await reviewRepository.UpdateReviewByID(review, id);
+});
+
+app.MapDelete("/api/reviews/{id}", async (IReviewRepository reviewRepository, int id) =>
+{
+  return await reviewRepository.DeleteReviewByID(id);
 });
 
 app.Run();
---
+++ week7/meal-sharing-final/backend/review/IReviewRepository.cs
@@ -0,0 +1,13 @@
+namespace mealSharingFfinal.review.IReviewRepository;
+using mealSharingFfinal.review.review;
+public interface IReviewRepository
+{
+  Task<IEnumerable<Review>> GetAllReview();
+
+  Task<Object> GetReviewByID(int id);
+  Task<Review> AddReview(Review review);
+  Task<Review> UpdateReviewByID(Review review, int id);
+  Task<Object> DeleteReviewByID(int id);
+  Task<IEnumerable<Review>> SearchReview(string title);
+
+}
